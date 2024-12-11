74a34fbeac67ce04c249fa50eedad880ea8266e1
Samayosri <samayosri2004@gmail.com>
2024-12-11 02:54:58 +0200

    admin test1

--- BackEnd/EmailSystem/src/main/java/repositories/AttachmentRepository.java
+++ BackEnd/EmailSystem/src/main/java/repositories/AttachmentRepository.java
@@ -2,7 +2,6 @@ package repositories;
 
 import entities.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- BackEnd/EmailSystem/src/main/java/repositories/MailRepository.java
+++ BackEnd/EmailSystem/src/main/java/repositories/MailRepository.java
@@ -2,7 +2,6 @@ package repositories;
 
 import entities.Mail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
